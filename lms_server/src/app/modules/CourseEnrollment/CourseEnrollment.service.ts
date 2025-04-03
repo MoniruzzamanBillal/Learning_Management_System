8bb62e01adf31be6f10532d1571e1047027f45a4
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-04-03 12:17:04 +0600

    payment gateway redirect link generated

--- lms_server/src/app/modules/CourseEnrollment/CourseEnrollment.service.ts
+++ lms_server/src/app/modules/CourseEnrollment/CourseEnrollment.service.ts
@@ -4,6 +4,7 @@ import { userModel } from "../user/user.model";
 import AppError from "../../Error/AppError";
 import httpStatus from "http-status";
 import { courseModel } from "../course/course.model";
+import { sslServices } from "../SSL/SSL.service";
 
 // ! for enrolling into a course
 const enrollInCourse = async (payload: { user: string; course: string }) => {
@@ -26,47 +27,19 @@ const enrollInCourse = async (payload: { user: string; course: string }) => {
 
   const transactionId = `TXN-${Date.now()}`;
 
-  const data = {
-    store_id: config.STORE_ID,
-    store_passwd: config.STORE_PASSWORD,
-    total_amount: courseData?.price,
-    currency: "BDT",
-    tran_id: transactionId,
-    success_url: "http://localhost:3030/success",
-    fail_url: "http://localhost:3030/fail",
-    cancel_url: "http://localhost:3030/cancel",
-    ipn_url: "http://localhost:3030/ipn",
-    shipping_method: "Courier",
-    product_name: courseData?.name,
-    product_category: courseData?.category,
-    product_profile: "general",
-    cus_name: userData?.name,
-    cus_email: userData?.email,
-    cus_add1: "N/A",
-    cus_add2: "N/A",
-    cus_city: "N/A",
-    cus_state: "N/A",
-    cus_postcode: "N/A",
-    cus_country: "Bangladesh",
-    cus_phone: "01711111111",
-    cus_fax: "01711111111",
-    ship_name: userData?.name,
-    ship_add1: "N/A",
-    ship_add2: "N/A",
-    ship_city: "N/A",
-    ship_state: "N/A",
-    ship_postcode: 1000,
-    ship_country: "Bangladesh",
+  const paymentData = {
+    price: courseData?.price,
+    transactionId,
+    productName: courseData?.name,
+    productCategory: courseData?.category,
+
+    userName: userData?.name,
+    userEmail: userData?.email,
   };
 
-  const response = await axios({
-    method: "post",
-    url: config.SSL_PAYMENT_URL,
-    data,
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  });
+  const result = await sslServices.initPayment(paymentData);
 
-  return response?.data;
+  return result;
 
   //
 };
