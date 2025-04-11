61b0abe176cb369e0bbc3faf5737168d7b32c28c
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-04-11 08:08:42 +0600

    frontend initialize

---
+++ lms_client/app/components/shared/Wrapper.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from "react";
+
+type TWrapperProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+const Wrapper = ({ children, className }: TWrapperProps) => {
+  return (
+    <div
+      className={`WrapperContainer  w-[96%] sm:w-[92%] xl:w-[90%] xlg:w-[85%] m-auto ${
+        className || ""
+      } `}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Wrapper;
