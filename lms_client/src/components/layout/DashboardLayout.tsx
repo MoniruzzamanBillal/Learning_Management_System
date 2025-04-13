66890a9a9ca0313b7df47dbc6f80103365748075
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-04-14 05:58:33 +0600

    dashboard layout added

--- lms_client/src/components/layout/DashboardLayout.tsx
+++ lms_client/src/components/layout/DashboardLayout.tsx
@@ -1,10 +1,23 @@
+import { Outlet } from "react-router-dom";
+import Wrapper from "../shared/Wrapper";
+import { Sidebar } from "../ui/Dashboard";
+
 const DashboardLayout = () => {
   return (
-    <div className="DashboardLayoutContainer">
-      <h1> DashboardLayout</h1>
-      <h1> DashboardLayout</h1>
-      <h1> DashboardLayout</h1>
-      <h1> DashboardLayout</h1>
+    <div className="DashboardLayoutContainer bg-gray-50 dark:bg-black50 ">
+      <Wrapper className="dashbaordWrapper min-h-screen mx-auto sm:flex py-4 px-2 gap-x-4 gap-y-6  ">
+        {/* sidebar section  */}
+
+        <div className="sideBarContainer  sm:w-64 ">
+          <Sidebar />
+        </div>
+        {/* sidebar section  */}
+
+        {/* children section  */}
+        <div className="contentSection w-full    ">
+          <Outlet />
+        </div>
+      </Wrapper>
     </div>
   );
 };
