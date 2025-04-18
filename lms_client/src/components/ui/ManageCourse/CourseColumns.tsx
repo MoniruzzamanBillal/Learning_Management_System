8b60b887284bea11b952fa33e9713b24644dcbbc
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-04-18 21:46:32 +0600

    manage course : sort based on price

--- lms_client/src/components/ui/ManageCourse/CourseColumns.tsx
+++ lms_client/src/components/ui/ManageCourse/CourseColumns.tsx
@@ -33,7 +33,19 @@ const courseColumns: ColumnDef<TDataprops>[] = [
   },
   {
     accessorKey: "price",
-    header: "Price",
+    header: ({ column }) => {
+      console.log(column);
+
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Price
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "published",
