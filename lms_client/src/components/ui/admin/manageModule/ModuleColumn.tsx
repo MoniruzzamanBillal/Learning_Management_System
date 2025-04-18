06309c14c61455e2553cc64b716298b0baa4ad58
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-04-19 05:40:31 +0600

    manage module column structure added

--- lms_client/src/components/ui/admin/manageModule/ModuleColumn.tsx
+++ lms_client/src/components/ui/admin/manageModule/ModuleColumn.tsx
@@ -1,9 +1,54 @@
-const ModuleColumn = () => {
-  return (
-    <div className="ModuleColumnContainer">
-      <h1> ModuleColumn</h1>
-    </div>
-  );
-};
+import { CourseModule } from "@/utils/DummyData";
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../../dropdown-menu";
+import { Button } from "../../button";
 
-export default ModuleColumn;
+const manageModuleColumns: ColumnDef<CourseModule>[] = [
+  {
+    accessorKey: "course",
+    header: "Course",
+  },
+  {
+    accessorKey: "modules",
+    header: "Modules",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const rowData = row?.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem onClick={() => console.log(rowData)}>
+              View Details
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => console.log(rowData?.id)}>
+              Update{" "}
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Delete Module</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
+
+export default manageModuleColumns;
