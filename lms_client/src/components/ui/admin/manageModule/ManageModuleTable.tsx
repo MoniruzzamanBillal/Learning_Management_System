aa4566a53b1a793fe3c962c2499a2b0ce56f1103
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-04-19 05:41:16 +0600

    manage module table structure added

--- lms_client/src/components/ui/admin/manageModule/ManageModuleTable.tsx
+++ lms_client/src/components/ui/admin/manageModule/ManageModuleTable.tsx
@@ -1,7 +1,123 @@
-const ManageModuleTable = () => {
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+} from "@tanstack/react-table";
+import { Button } from "../../button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../../table";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+const ManageModuleTable = <TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) => {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    initialState: {
+      pagination: {
+        pageSize: 5,
+      },
+    },
+  });
+
   return (
-    <div className="ManageModuleTableContainer">
-      <h1> ManageModuleTable</h1>
+    <div className="rounded-md  bg-gray-50 border  border-gray-200 ">
+      <Table className="  ">
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => {
+                  const cellValue = cell.getValue();
+
+                  return (
+                    <>
+                      <TableCell key={cell.id}>
+                        {typeof cellValue === "string" ? (
+                          cellValue
+                        ) : Array.isArray(cellValue) ? (
+                          <ul className="list-disc pl-4">
+                            {cellValue?.map((cellItem, index) => (
+                              <li key={index}>{cellItem?.name}</li>
+                            ))}
+                          </ul>
+                        ) : (
+                          flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )
+                        )}
+                      </TableCell>
+                    </>
+                  );
+                })}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
     </div>
   );
 };
