c583535198c25e9c2280b7fb60ae174d0f024d41
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-05-01 07:37:08 +0600

    assign course detail page : dynamically course data show

--- lms_client/src/pages/Dashboard/instructor/AssignCourseDetail.tsx
+++ lms_client/src/pages/Dashboard/instructor/AssignCourseDetail.tsx
@@ -6,9 +6,11 @@ import {
 } from "@/components/ui/instructor/AssignCourseDetailTable";
 import { useGetCourseDetailsForInstructorQuery } from "@/redux/features/course/course.api";
 import { useGetModuleFromCourseIdQuery } from "@/redux/features/module/module.api";
-import { Link, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const AssignCourseDetail = () => {
+  const navigate = useNavigate();
+
   const { courseId } = useParams();
 
   if (!courseId) {
@@ -22,11 +24,11 @@ const AssignCourseDetail = () => {
     useGetModuleFromCourseIdQuery(courseId, { skip: !courseId });
 
   // console.log(courseDetail?.data);
-  console.log(courseDetailWithModule?.data);
+  // console.log(courseDetailWithModule?.data);
 
   return (
     <>
-      {moduleDataLoading && <TableDataLoading />}
+      {(courseDetailLoading || moduleDataLoading) && <TableDataLoading />}
 
       <div className="AssignCourseDetailContainer">
         <div className="AssignCourseDetailWrapper bg-gray-100 border border-gray-300  shadow rounded-md p-4 ">
@@ -38,29 +40,50 @@ const AssignCourseDetail = () => {
             <div className="bodyLeft text-lg flex flex-col gap-y-2">
               {/* course name  */}
               <p className="courseName">
-                <span className=" font-bold ">Course name : </span> Mastering
-                Frontend Development with js
+                <span className=" font-bold ">Course name : </span>{" "}
+                {courseDetail?.data?.name}
               </p>
 
               {/* course published */}
               <p className="coursePublished">
-                <span className=" font-bold ">Course Published : </span> false
+                <span className={` font-bold  `}>Course Published : </span>{" "}
+                <span
+                  className={` ${
+                    courseDetail?.data?.published
+                      ? "text-green-600 "
+                      : "text-red-600 "
+                  } `}
+                >
+                  {courseDetail?.data?.published
+                    ? "Published "
+                    : "Unpublished "}
+                </span>
               </p>
 
               {/* course category  */}
               <p className="courseCategory">
-                <span className=" font-bold ">Course category : </span> Web
-                Development
+                <span className=" font-bold  ">Course category : </span>{" "}
+                {courseDetail?.data?.category}
               </p>
             </div>
 
             {/* add  button section  */}
             <div className=" rightSection btnSection   ">
-              <Link to={"/dashboard/instructor/add-module"}>
-                <Button className=" bg-prime100 hover:bg-prime200 ">
-                  Add New Module
-                </Button>
-              </Link>
+              <Button
+                disabled={courseDetail?.data?.published}
+                onClick={() =>
+                  navigate(
+                    `/dashboard/instructor/add-module?courseId=${courseDetail?.data?._id}`
+                  )
+                }
+                className={`    ${
+                  courseDetail?.data?.published
+                    ? "bg-gray-700  "
+                    : " bg-prime100 hover:bg-prime200"
+                } `}
+              >
+                Add New Module
+              </Button>
             </div>
 
             {/*  */}
