69dcbc3fdf71a544f4d58a6a0eafd4c2a1ca78e2
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-05-04 05:31:26 +0600

    my course card dynamically data show

--- lms_client/src/components/ui/user/MyCourses/MyCourseCard.tsx
+++ lms_client/src/components/ui/user/MyCourses/MyCourseCard.tsx
@@ -1,12 +1,50 @@
-const MyCourseCard = () => {
+import { TUserEnrolledCourse } from "@/pages/user/MyCourses";
+import { Link } from "react-router-dom";
+import { Button } from "../../button";
+import { Progress } from "../../progress";
+
+const MyCourseCard = ({ courseData }: { courseData: TUserEnrolledCourse }) => {
   return (
-    <div className="MyCourseCardContainer">
-      <h1> MyCourseCard</h1>
-      <h1> MyCourseCard</h1>
-      <h1> MyCourseCard</h1>
-      <h1> MyCourseCard</h1>
-      <h1> MyCourseCard</h1>
-      <h1> MyCourseCard</h1>
+    <div className="enrolledCourseCard">
+      <div className="enrolledCourseCardWrapper  w-[75%] rounded-md shadow bg-white border border-gray-200 flex justify-between gap-x-5  ">
+        {/* left course cover section  */}
+        <div className="courseLeftCover w-[40%] h-[13rem] rounded-l-md overflow-auto ">
+          <img
+            src={courseData?.course?.courseCover}
+            className=" w-full h-full "
+            alt={courseData?.course?.name}
+          />
+        </div>
+        {/*  */}
+
+        {/* course detail section  */}
+        <div className="detailSection rightSection py-2 w-[60%] flex flex-col gap-y-3  pr-3  ">
+          {/* courseName */}
+          <p className=" text-2xl font-medium ">{courseData?.course?.name}</p>
+          {/* platform name  */}
+          <p className=" text-lg ">MATS academy </p>
+
+          {/* course progress section  */}
+          <div className="courseProgressSection w-[90%] flex justify-center items-center gap-x-6  ">
+            <Progress value={courseData?.courseProgress} className="  " />
+            <p className=" text-lg font-medium ">
+              {courseData?.courseProgress}%
+            </p>
+          </div>
+
+          {/* button section  */}
+
+          <Link to={`/user/course-detail/${courseData?.course?._id}`}>
+            <Button className=" bg-prime100 hover:bg-prime200 ">
+              Continue
+            </Button>
+          </Link>
+        </div>
+
+        {/*  */}
+      </div>
+
+      {/*  */}
     </div>
   );
 };
