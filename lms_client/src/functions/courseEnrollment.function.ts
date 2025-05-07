03ca52d7468114ac3d4443d91099a1e7750f864f
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-05-07 18:02:30 +0600

    course enrollment functionality done

---
+++ lms_client/src/functions/courseEnrollment.function.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { toast } from "sonner";
+
+type TenrollCourse = {
+  user: string;
+  course: string;
+};
+
+// ! for enrolling into a course
+export const enrollInCourseFunction = async (
+  payload: TenrollCourse,
+  enrollInCourse: any
+) => {
+  const taostId = toast.loading("Purchasing Course....");
+
+  try {
+    const result = await enrollInCourse(payload);
+
+    // console.log(result?.data?.data);
+
+    if (result?.error) {
+      const errorMessage = (result?.error as any)?.data?.message;
+      console.log(errorMessage);
+      toast.error(errorMessage, {
+        id: taostId,
+        duration: 1400,
+      });
+    }
+
+    if (result?.data) {
+      const successMsg = result?.data?.message;
+      const paymentUrl = result?.data?.data;
+
+      toast.success(successMsg, {
+        id: taostId,
+        duration: 2000,
+      });
+
+      window.location.href = paymentUrl;
+    }
+  } catch (error) {
+    console.log(error);
+    toast.error("Something went wrong while purchasing the course !!!", {
+      duration: 1400,
+    });
+  }
+};
