2b7f0523d4171394d3bd5820833054f9de511612
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-05-07 10:15:09 +0600

    course detail page : course right cover secton added

--- lms_client/src/pages/CourseDetail.tsx
+++ lms_client/src/pages/CourseDetail.tsx
@@ -1,4 +1,5 @@
 import Wrapper from "@/components/shared/Wrapper";
+import { Button } from "@/components/ui/button";
 import { useParams } from "react-router-dom";
 
 const CourseDetail = () => {
@@ -7,30 +8,59 @@ const CourseDetail = () => {
   console.log("course id = ", courseId);
 
   return (
-    <div className="CourseDetailContainer">
+    <div className="CourseDetailContainer bg-gray-50 min-h-screen ">
       <div className="CourseDetailWrapper">
         {/* corse detail top name section  */}
         <div className="courseTopSection  py-8 bg-gray-200  ">
-          <Wrapper className="courseNameSection   flex flex-col gap-y-4  ">
-            {/* course name  */}
-            <p className=" text-3xl font-semibold ">Course name </p>
+          <Wrapper className="courseNameTopSection   flex justify-between gap-x-3     ">
+            {/* course name section  */}
+            <div className="courseNameSection  w-[70%] flex flex-col gap-y-4 ">
+              {/* course name  */}
+              <p className=" text-3xl font-semibold ">Course name </p>
 
-            <p className=" text-lg font-medium "> Course Level : Beginner</p>
+              <p className=" text-lg font-medium "> Course Level : Beginner</p>
 
-            {/* course instructors  */}
-            <div className="courseInstructors">
-              <p className=" text-lg font-semibold ">Course instructors : </p>
-              <ul className=" list-inside list-decimal ">
-                <li className=" pl-3 font-medium text-prime100 ">
-                  Instructor 1
-                </li>
-                <li className=" pl-3 ">Instructor 2</li>
-              </ul>
+              {/* course instructors  */}
+              <div className="courseInstructors">
+                <p className=" text-lg font-semibold ">Course instructors : </p>
+                <ul className=" list-inside list-decimal ">
+                  <li className=" pl-3 font-medium text-prime100 ">
+                    Instructor 1
+                  </li>
+                  <li className=" pl-3 ">Instructor 2</li>
+                </ul>
+              </div>
+
+              <p className=" text-lg font-medium ">Total Modules : 3 </p>
+
+              <p className=" text-lg font-medium ">Last update : 1 may 2025</p>
+
+              {/*  */}
             </div>
 
-            <p className=" text-lg font-medium ">Total Modules : 3 </p>
+            {/* course cover , price  section  */}
+            <div className="courseCoverImg  w-[30%] border border-gray-200 rounded bg-gray-50 shadow p-3 flex flex-col gap-y-3 ">
+              {/* image section  */}
+              <div className="courseCover h-[12rem] rounded-t overflow-auto">
+                <img
+                  src="https://i.postimg.cc/T3gDnj7S/494358010-646246441651486-5487058506954483020-n.jpg"
+                  className=" w-full h-full "
+                  alt="course_cover"
+                />
+              </div>
 
-            <p className=" text-lg font-medium ">Last update : 1 may 2025</p>
+              {/* course price section  */}
+              <p className=" py-2 text-2xl font-semibold text-prime200 ">
+                Price : $120
+              </p>
+
+              {/* enroll button  */}
+              <Button className="  bg-prime100 hover:bg-prime200 ">
+                Enroll Now{" "}
+              </Button>
+
+              {/*  */}
+            </div>
 
             {/*  */}
           </Wrapper>
