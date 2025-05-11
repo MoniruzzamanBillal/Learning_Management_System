519be35e01bdc40c9aa2ee9c621644570897a7a4
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-05-11 08:32:47 +0600

    review model added in backend

---
+++ lms_server/src/app/modules/review/review.model.ts
@@ -0,0 +1,43 @@
+import { model, Schema } from "mongoose";
+import { TReview } from "./review.interface";
+
+const reviewSchema = new Schema<TReview>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User is required "],
+    },
+    courseId: {
+      type: Schema.Types.ObjectId,
+      ref: "Course",
+      required: [true, "Course is required "],
+    },
+    rating: {
+      type: Number,
+      required: [true, "Rating is required !!"],
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+      required: [true, "Comment is required !!"],
+    },
+  },
+  { timestamps: true }
+);
+
+reviewSchema.pre("find", async function (next) {
+  this.where({ isDeleted: false });
+
+  next();
+});
+
+reviewSchema.pre("findOne", async function (next) {
+  this.where({ isDeleted: false });
+
+  next();
+});
+
+//
+export const reviewModel = model<TReview>("Review", reviewSchema);
