5ac2ef8267c9864a76fff36dbb45d0927be1ccd9
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-05-21 11:55:42 +0600

    homw page : course responsive

--- lms_client/src/components/ui/Home/PopularCourse.tsx
+++ lms_client/src/components/ui/Home/PopularCourse.tsx
@@ -10,18 +10,18 @@ const PopularCourse = () => {
   return (
     <div className="PopularCourseContainer bg-gray-50 py-8 ">
       <Wrapper className="PopularCourseWrapper">
-        <h1 className=" text-center text-prime100 font-medium ">
+        <h1 className=" text-center text-prime100 font-medium  text-xl xsm:text-2xl md:text-3xl">
           Our Most Popular Class
         </h1>
 
-        <h2 className=" text-center pt-4 mb-8 font-medium ">
+        <h2 className=" text-center pt-4 mb-8 font-medium text-lg xsm:text-xl md:text-2xl ">
           Let's join our famous class, the knowledge provided will definitely be
           useful for you.
         </h2>
 
-        <div className="courseSection grid grid-cols-3 gap-x-4 gap-y-6">
+        <div className="courseSection  m-auto  w-[88%] xsm:w-[70%]  sm:w-full  grid grid-cols-1 sm:grid-cols-2 xmd:grid-cols-3 gap-x-4 gap-y-6">
           {courseDataLoading &&
-            Array.from({ length: 6 })?.map((_, ind) => (
+            Array.from({ length: 3 })?.map((_, ind) => (
               <CourseCardSkeleton key={ind} />
             ))}
 
