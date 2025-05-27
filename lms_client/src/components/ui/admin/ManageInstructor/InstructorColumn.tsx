08f6885b657ec1bfa249445b501336113cef0167
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-05-27 07:55:25 +0600

    instructor column update

--- lms_client/src/components/ui/admin/ManageInstructor/InstructorColumn.tsx
+++ lms_client/src/components/ui/admin/ManageInstructor/InstructorColumn.tsx
@@ -1,16 +1,7 @@
 import { TInstructorDummyData } from "@/components/TestingTable/DummyData";
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown, MoreHorizontal } from "lucide-react";
-import { Link } from "react-router-dom";
+import { ArrowUpDown } from "lucide-react";
 import { Button } from "../../button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "../../dropdown-menu";
 
 const InstructorColumn: ColumnDef<TInstructorDummyData>[] = [
   {
@@ -44,38 +35,6 @@ const InstructorColumn: ColumnDef<TInstructorDummyData>[] = [
   {
     accessorKey: "profilePicture",
     header: "Profile Image",
-  },
-  {
-    accessorKey: "Action",
-    id: "actions",
-    cell: ({ row }) => {
-      const rowData = row?.original;
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem>
-              <Link to={"/dashboard/admin/instructor-detail/1234"}>
-                View Details
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <Link to={`/dashboard/admin/update-instructor/${rowData?.id}`}>
-                Update Instructor
-              </Link>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
   },
 ];
 
