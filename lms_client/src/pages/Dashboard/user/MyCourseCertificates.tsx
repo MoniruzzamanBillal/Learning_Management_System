6c6fd2f5861399b4c1cecf32e6ef16ad0cca01ab
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-06-03 22:53:57 +0600

    certificate page : loading satte skeleton show fix

--- lms_client/src/pages/Dashboard/user/MyCourseCertificates.tsx
+++ lms_client/src/pages/Dashboard/user/MyCourseCertificates.tsx
@@ -9,6 +9,25 @@ const MyCourseCertificates = () => {
   const { data: userFinishedCourse, isLoading } =
     useGetUserFinishCourseQuery(undefined);
 
+  let content = null;
+
+  if (!userFinishedCourse?.data?.length && !isLoading) {
+    content = (
+      <h1 className=" text-prime100 text-center  py-16 ">
+        User did not finished any course yet!!!
+      </h1>
+    );
+  } else if (userFinishedCourse?.data?.length) {
+    content = (
+      <div className="Tablecontainer mx-auto py-10">
+        <CourseCertificateTable
+          columns={CertificateTableColumn}
+          data={userFinishedCourse?.data}
+        />
+      </div>
+    );
+  }
+
   return (
     <div className="MyCourseCertificatesContainer">
       <div className="MyCourseCertificatesWrapper bg-gray-100/90 border border-gray-300  shadow rounded-md p-3 ">
@@ -18,20 +37,7 @@ const MyCourseCertificates = () => {
 
         {isLoading && <TableDataLoading />}
 
-        <div className="courseCertificates  ">
-          {userFinishedCourse?.data?.length ? (
-            <div className="Tablecontainer mx-auto py-10">
-              <CourseCertificateTable
-                columns={CertificateTableColumn}
-                data={userFinishedCourse?.data}
-              />
-            </div>
-          ) : (
-            <h1 className=" text-prime100 text-center  py-16 ">
-              User did not finished any course yet!!!
-            </h1>
-          )}
-        </div>
+        <div className="courseCertificates  ">{content}</div>
       </div>
     </div>
   );
