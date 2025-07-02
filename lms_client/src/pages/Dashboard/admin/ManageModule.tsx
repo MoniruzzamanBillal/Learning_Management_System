cd6a269e05a9fc5b4ad31101508688b1e1cc91a4
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-07-02 21:43:10 +0600

    manage module table loading skeleton added

--- lms_client/src/pages/Dashboard/admin/ManageModule.tsx
+++ lms_client/src/pages/Dashboard/admin/ManageModule.tsx
@@ -11,27 +11,29 @@ const ManageModule = () => {
 
   // console.log(moduleDataWithCourse?.data);
 
+  let content = null;
+
+  if (isLoading) {
+    content = <TableDataLoading />;
+  } else if (moduleDataWithCourse) {
+    content = moduleDataWithCourse && moduleDataWithCourse?.data && (
+      <ManageModuleTable
+        columns={manageModuleColumns}
+        data={moduleDataWithCourse?.data}
+      />
+    );
+  }
+
   return (
-    <>
-      {isLoading && <TableDataLoading />}
+    <div className="ManageModuleContainer">
+      <div className="ManageModuleWrapper bg-gray-100/90 border border-gray-300  shadow rounded-md p-3">
+        <h3 className="brand text-2xl font-medium mb-4 "> Manage Module </h3>
+        {/*  */}
 
-      <div className="ManageModuleContainer">
-        <div className="ManageModuleWrapper bg-gray-100/90 border border-gray-300  shadow rounded-md p-3">
-          <h3 className="brand text-2xl font-medium mb-4 "> Manage Module </h3>
-          {/*  */}
-
-          {/* table section  */}
-          <div className="Tablecontainer mx-auto py-10">
-            {moduleDataWithCourse?.data && (
-              <ManageModuleTable
-                columns={manageModuleColumns}
-                data={moduleDataWithCourse?.data}
-              />
-            )}
-          </div>
-        </div>
+        {/* table section  */}
+        <div className="Tablecontainer mx-auto py-10">{content}</div>
       </div>
-    </>
+    </div>
   );
 };
 
