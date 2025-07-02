95af00a60f2113b13a018459b19a8c0edf272252
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-07-02 21:40:27 +0600

    admin manage course detail section : table loading skeleton added

--- lms_client/src/pages/Dashboard/admin/CourseDetail.tsx
+++ lms_client/src/pages/Dashboard/admin/CourseDetail.tsx
@@ -24,8 +24,6 @@ const CourseDetail = () => {
 
   const { courseId } = useParams();
 
-  // console.log("course id = ", courseId);
-
   if (!courseId) {
     throw new Error("Something went wrong !!!!");
   }
@@ -41,8 +39,6 @@ const CourseDetail = () => {
     skip: !courseId,
   });
 
-  // console.log(courseData?.data);
-
   // ! for publishing a course
   const publishAdminCourse = async () => {
     const result = await publishCourseFunction(courseId, publishCourse);
@@ -52,154 +48,170 @@ const CourseDetail = () => {
     }
   };
 
-  return (
-    <>
-      {courseDataLoading && <TableDataLoading />}
+  let courseDetailContent = null;
+  let instructorTableSection = null;
+  let moduleTableSection = null;
 
-      <div className="CourseDetailContainer">
-        <div className="CourseDetailWrapper bg-gray-100 border border-gray-300  shadow rounded-md p-3 ">
-          <h3 className="brand text-3xl font-medium mb-4 underline  ">
-            Course Detail :
-          </h3>
-
-          <div className="courseDetailBody">
-            {/* heading section  */}
-            <div className="headingSection mb-8 flex justify-between items-center">
-              <div className="headingLeft">
-                <h1 className=" font-medium text-xl  ">
-                  {" "}
-                  Course id : #{courseId}{" "}
-                </h1>
-              </div>
-
-              <div className=" rightSection btnSection flex justify-between items-center gap-x-4  ">
-                {!courseData?.data?.published && (
-                  <Button
-                    disabled={coursepublishingLoading}
-                    className=" bg-prime100 hover:bg-prime200 "
-                    onClick={() => publishAdminCourse()}
-                  >
-                    {coursepublishingLoading
-                      ? "Publishing course..."
-                      : " Publish Course"}
-                  </Button>
-                )}
-
-                <Button
-                  disabled={courseData?.data?.published}
-                  onClick={() =>
-                    navigate(
-                      `/dashboard/admin/update-course/${courseData?.data?._id}`
-                    )
-                  }
-                  className={`    ${
-                    courseData?.data?.published
-                      ? "bg-gray-700  "
-                      : " bg-green-600 hover:bg-green-700"
-                  } `}
-                >
-                  Update Course
-                </Button>
-              </div>
-
-              {/*  */}
-            </div>
-
-            {/* course detail body  */}
-            <div className="detailBody text-lg ">
-              {/* course name  */}
-              <p className="courseName">
-                <span className=" font-bold ">Course name : </span>{" "}
-                {courseData?.data?.name}
-              </p>
-
-              {/* course category  */}
-              <p className="courseCategory">
-                <span className=" font-bold ">Course category : </span>{" "}
-                {courseData?.data?.category}
-              </p>
-
-              {/* course price */}
-              <p className="coursePrice">
-                <span className=" font-bold ">Course price : </span>{" "}
-                {courseData?.data?.price}
-              </p>
-
-              {/* course published */}
-              <p className="coursePublished">
-                <span className={` font-bold  `}>Course Published : </span>{" "}
-                <span
-                  className={` ${
-                    courseData?.data?.published
-                      ? "text-green-600 "
-                      : "text-red-600 "
-                  } `}
-                >
-                  {courseData?.data?.published ? "Published " : "Unpublished "}
-                </span>
-              </p>
-
-              {/* course detail */}
-
-              <div className="courseDetail">
-                <Accordion type="single" collapsible className="w-full">
-                  <AccordionItem value="item-1">
-                    <AccordionTrigger className=" font-bold ">
-                      Course details
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      <div
-                        className="  courseDetail "
-                        dangerouslySetInnerHTML={{
-                          __html: courseData?.data?.description,
-                        }}
-                      ></div>
-                    </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
-              </div>
-
-              {/*  */}
-            </div>
-
-            {/* table section  */}
-            {/* instructors table  */}
-            <div className="instructorsTable pt-8 ">
-              <h1 className=" font-medium text-xl mb-2 underline ">
-                Instructors :
-              </h1>
-
-              {courseData?.data?.instructors && (
-                <div className="TableContainer mx-auto mt-2">
-                  <InstructorTable
-                    columns={InstructorColumn}
-                    data={courseData?.data?.instructors}
-                  />
-                </div>
-              )}
-            </div>
-
-            {/* module table  */}
-            <div className="modulesTable pt-8 ">
-              <h1 className=" font-medium text-xl mb-2 underline ">
-                Modules :
-              </h1>
-
-              {courseData?.data?.modules && (
-                <div className="TableContainer mx-auto mt-2">
-                  <CourseDetailModuleTable
-                    columns={CourseDetailModuleColumn}
-                    data={courseData?.data?.modules}
-                  />
-                </div>
-              )}
-            </div>
-
-            {/*  */}
+  if (courseDataLoading) {
+    courseDetailContent = <TableDataLoading />;
+  }
+  if (courseData) {
+    courseDetailContent = (
+      <div className="courseDetailSection">
+        {/* heading section  */}
+        <div className="headingSection mb-8 flex justify-between items-center">
+          <div className="headingLeft">
+            <h1 className=" font-medium text-xl  ">
+              {" "}
+              Course id : #{courseId}{" "}
+            </h1>
           </div>
+
+          <div className=" rightSection btnSection flex justify-between items-center gap-x-4  ">
+            {!courseData?.data?.published && (
+              <Button
+                disabled={coursepublishingLoading}
+                className=" bg-prime100 hover:bg-prime200 "
+                onClick={() => publishAdminCourse()}
+              >
+                {coursepublishingLoading
+                  ? "Publishing course..."
+                  : " Publish Course"}
+              </Button>
+            )}
+
+            <Button
+              disabled={courseData?.data?.published}
+              onClick={() =>
+                navigate(
+                  `/dashboard/admin/update-course/${courseData?.data?._id}`
+                )
+              }
+              className={`    ${
+                courseData?.data?.published
+                  ? "bg-gray-700  "
+                  : " bg-green-600 hover:bg-green-700"
+              } `}
+            >
+              Update Course
+            </Button>
+          </div>
+
+          {/*  */}
+        </div>
+
+        {/* course detail body  */}
+        <div className="detailBody text-lg ">
+          {/* course name  */}
+          <p className="courseName">
+            <span className=" font-bold ">Course name : </span>{" "}
+            {courseData?.data?.name}
+          </p>
+
+          {/* course category  */}
+          <p className="courseCategory">
+            <span className=" font-bold ">Course category : </span>{" "}
+            {courseData?.data?.category}
+          </p>
+
+          {/* course price */}
+          <p className="coursePrice">
+            <span className=" font-bold ">Course price : </span>{" "}
+            {courseData?.data?.price}
+          </p>
+
+          {/* course published */}
+          <p className="coursePublished">
+            <span className={` font-bold  `}>Course Published : </span>{" "}
+            <span
+              className={` ${
+                courseData?.data?.published
+                  ? "text-green-600 "
+                  : "text-red-600 "
+              } `}
+            >
+              {courseData?.data?.published ? "Published " : "Unpublished "}
+            </span>
+          </p>
+
+          {/* course detail */}
+
+          <div className="courseDetail">
+            <Accordion type="single" collapsible className="w-full">
+              <AccordionItem value="item-1">
+                <AccordionTrigger className=" font-bold ">
+                  Course details
+                </AccordionTrigger>
+                <AccordionContent>
+                  <div
+                    className="  courseDetail "
+                    dangerouslySetInnerHTML={{
+                      __html: courseData?.data?.description,
+                    }}
+                  ></div>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
+
+          {/*  */}
         </div>
       </div>
-    </>
+    );
+
+    // instructor table section
+    instructorTableSection = (
+      <div className="instructorsTable pt-8 ">
+        <h1 className=" font-medium text-xl mb-2 underline ">Instructors :</h1>
+
+        {courseData?.data?.instructors && (
+          <div className="TableContainer mx-auto mt-2">
+            <InstructorTable
+              columns={InstructorColumn}
+              data={courseData?.data?.instructors}
+            />
+          </div>
+        )}
+      </div>
+    );
+
+    moduleTableSection = (
+      <div className="modulesTable pt-8 ">
+        <h1 className=" font-medium text-xl mb-2 underline ">Modules :</h1>
+
+        {courseData?.data?.modules && (
+          <div className="TableContainer mx-auto mt-2">
+            <CourseDetailModuleTable
+              columns={CourseDetailModuleColumn}
+              data={courseData?.data?.modules}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  return (
+    <div className="CourseDetailContainer">
+      <div className="CourseDetailWrapper bg-gray-100 border border-gray-300  shadow rounded-md p-3 ">
+        <h3 className="brand text-3xl font-medium mb-4 underline  ">
+          Course Detail :
+        </h3>
+
+        <div className="courseDetailBody">
+          {/* detail section  */}
+          {courseDetailContent}
+
+          {/* instructor table section  */}
+          {instructorTableSection}
+
+          {/* module table  */}
+          {moduleTableSection}
+
+          {/*  */}
+        </div>
+      </div>
+    </div>
   );
 };
 
