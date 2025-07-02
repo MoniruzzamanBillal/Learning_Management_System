3e05df529593343a411fc201894718f92da2a65b
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-07-02 21:33:15 +0600

    manage course loading skeleton added

--- lms_client/src/pages/Dashboard/admin/ManageCourse.tsx
+++ lms_client/src/pages/Dashboard/admin/ManageCourse.tsx
@@ -11,41 +11,35 @@ import { useNavigate } from "react-router-dom";
 const ManageCourse = () => {
   const navigate = useNavigate();
 
-  // const { data: allCourseData, isLoading: courseDataLoading } =
-  //   useGetAllCourseAdminQuery(undefined, { refetchOnMountOrArgChange: true });
-
   const { data: allCourseData, isLoading: courseDataLoading } =
     useGetAllCourseAdminQuery(undefined);
 
-  // console.log(allCourseData?.data);
+  let content = null;
+
+  if (courseDataLoading) {
+    content = <TableDataLoading />;
+  } else if (allCourseData) {
+    content = allCourseData && allCourseData?.data && (
+      <ManageCourseTable columns={CourseColumns} data={allCourseData?.data} />
+    );
+  }
 
   return (
-    <>
-      {courseDataLoading && <TableDataLoading />}
+    <div className="ManageCourseContainer">
+      <div className="manageCourseWrapper bg-gray-100/90 border border-gray-300  shadow rounded-md p-3 ">
+        <h3 className="brand text-2xl font-medium mb-4 "> Manage Course </h3>
 
-      <div className="ManageCourseContainer">
-        <div className="manageCourseWrapper bg-gray-100/90 border border-gray-300  shadow rounded-md p-3 ">
-          <h3 className="brand text-2xl font-medium mb-4 "> Manage Course </h3>
+        <Button
+          onClick={() => navigate("/dashboard/admin/add-course")}
+          className="mb-4 bg-prime100 hover:bg-prime200 cursor-pointer"
+        >
+          Add Course
+        </Button>
 
-          <Button
-            onClick={() => navigate("/dashboard/admin/add-course")}
-            className="mb-4 bg-prime100 hover:bg-prime200 cursor-pointer"
-          >
-            Add Course
-          </Button>
-
-          {/* table section  */}
-          <div className="Tablecontainer mx-auto py-10">
-            {allCourseData?.data && (
-              <ManageCourseTable
-                columns={CourseColumns}
-                data={allCourseData?.data}
-              />
-            )}
-          </div>
-        </div>
+        {/* table section  */}
+        <div className="Tablecontainer mx-auto py-10">{content}</div>
       </div>
-    </>
+    </div>
   );
 };
 
