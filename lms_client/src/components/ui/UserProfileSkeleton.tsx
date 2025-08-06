38aa7121cbd7cad29e34d28c93fa3af8d37198de
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-08-06 18:21:04 +0600

    profile page updated

--- lms_client/src/components/ui/UserProfileSkeleton.tsx
+++ lms_client/src/components/ui/UserProfileSkeleton.tsx
@@ -2,29 +2,32 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const UserProfileSkeleton = () => {
   return (
-    <div className="UserProfileContainer">
-      <div className="UserProfileWrapper bg-gray-100/90 border border-gray-300 shadow rounded-md p-3">
-        <div className="profileImgWrapper flex flex-col xsm:flex-row justify-between items-center gap-y-8">
-          {/* Left Section */}
-          <div className="profileLeftSection flex items-center gap-x-5">
-            {/* Image Skeleton */}
-            <div className="imgSection rounded-full overflow-hidden size-[9rem]">
-              <Skeleton className="w-full h-full rounded-full bg-gray-300 " />
-            </div>
+    <div className="UserProfileContainer w-full bg-gray-100">
+      {/* Cover Photo Skeleton */}
+      <Skeleton className="w-full h-48 sm:h-64 md:h-80 bg-gray-200" />
 
-            {/* Name & Email Skeleton */}
-            <div className="nameSection">
-              <div className="nameTopSection flex items-center gap-x-2 mb-2">
-                <Skeleton className="h-6 w-[150px] bg-gray-300 " />
-              </div>
-              <Skeleton className="h-4 w-[200px] bg-gray-300 " />
-            </div>
-          </div>
+      <div className="container mx-auto px-4 -mt-16 sm:-mt-24 md:-mt-32 relative z-10">
+        <div className="flex flex-col items-center gap-4">
+          {" "}
+          {/* Changed to items-center */}
+          {/* Profile Picture Skeleton */}
+          <Skeleton className="size-32 sm:size-40 md:size-48 rounded-full border-4 border-white bg-gray-300 shadow-lg" />
+          {/* Name Skeleton */}
+          <Skeleton className="h-8 w-48 mt-4" /> {/* Removed sm:mt-0 sm:ml-4 */}
+        </div>
 
-          {/* Edit Button Skeleton */}
-          <div className="profileRightSection">
-            <Skeleton className="h-10 w-[120px] bg-gray-300 " />
-          </div>
+        <div className="bg-white p-6 sm:p-8 rounded-lg shadow-lg mt-8 space-y-4">
+          {/* Email Skeleton */}
+          <Skeleton className="h-5 w-64" />
+          {/* Bio Skeleton */}
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-5/6" />
+          {/* Website Skeleton */}
+          <Skeleton className="h-4 w-48" />
+          {/* Joined On Skeleton */}
+          <Skeleton className="h-5 w-56" />
+          {/* Edit Profile Button Skeleton */}
+          <Skeleton className="h-10 w-32 rounded-md" />
         </div>
       </div>
     </div>
